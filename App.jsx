59b491c30a29a44d60692b883875e7d9b8736558
bhaskar165470 <115618841+bhaskar165470@users.noreply.github.com>
2026-02-25 08:15:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import { CartProvider } from "./context/CartContext";
+
+function App() {
+  return (
+    <CartProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </CartProvider>
+  );
+}
+
+export default App;
